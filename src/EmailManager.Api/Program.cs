a360f086a2bb79c097ddddcdd114ec1b4a0a594f
DaviPac <davipadecarvalho@hotmail.com>
2026-06-26 23:26:52 -0300

    feat: add CORS

--- src/EmailManager.Api/Program.cs
+++ src/EmailManager.Api/Program.cs
@@ -9,6 +9,17 @@ var port = Environment.GetEnvironmentVariable("PORT");
 if (port is not null)
     builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
+var allowedOrigins = builder.Configuration
+    .GetSection("Cors:AllowedOrigins").Get<string[]>() ?? [];
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Frontend", policy =>
+        policy.WithOrigins(allowedOrigins)
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
 builder.Services.AddControllers();
 builder.Services.AddInfrastructure(builder.Configuration);
 
@@ -24,6 +35,8 @@ using (var scope = app.Services.CreateScope())
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
 }
+
+app.UseCors("Frontend");
 
 app.MapIdentityApi<IdentityUser>();
 app.MapControllers();
